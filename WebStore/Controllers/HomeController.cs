7db9d2bc3270bb062f24a912aefc1fe2f05ae9ab
Victor Kalnik <v.kalnik@mail.ru>
2021-09-18 00:35:45 +0300

    Выделение работы с сотрудниками в отдельный контроллер с отдельным представлением

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -6,29 +6,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-        };
-
-        public IActionResult Index() // http://locaalhost:5000/Home/Index
-        {
-            //return Content("Hello from first controller!");
-            //return View("Index");
-            return View();
-        }
-
-        public IActionResult SecondAction(int id)
-        {
-            return Content($"Second action with parameter {id}");
-        }
-
-        public IActionResult Employees() // http://localhost:5000/Home/Employees
-        {
-            return View(__Employees);
-        }
+        public IActionResult Index() => View();
     }
 }
