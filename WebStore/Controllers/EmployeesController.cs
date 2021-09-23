7d0b5916e3308d3946835919a086c4d87117f4e4
Victor Kalnik <v.kalnik@mail.ru>
2021-09-23 16:42:07 +0300

    Примеры ручной маршрутизации в контроллерах

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -6,6 +6,8 @@ using WebStore.Models;
 
 namespace WebStore.Controllers
 {
+    //[Route("Employrrs/[action]/{id?}")]
+    //[Route("Staff/[action]/{id?}")]
     public class EmployeesController : Controller
     {
         private readonly IEnumerable<Employee> _Employees;
@@ -15,8 +17,10 @@ namespace WebStore.Controllers
             _Employees = TestData.Employees;
         }
 
+        //[Route("~/employees/all")]
         public IActionResult Index() => View(_Employees);
 
+        //[Route("~/employees/all/info-{id}")]
         public IActionResult Details(int id)
         {
             //var employee = _Employees.FirstOrDefault(e => e.Id == id);
