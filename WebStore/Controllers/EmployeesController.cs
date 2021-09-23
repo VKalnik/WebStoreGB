d7a968d5b82f43fdfeea4fc8bbaf99d80c24cb58
Victor Kalnik <v.kalnik@mail.ru>
2021-09-23 18:48:09 +0300

    CRUD-интерфейс в контроллере сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebStore.Data;
+using WebStore.Models;
 using WebStore.Sevices.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -29,5 +32,80 @@ namespace WebStore.Controllers
 
             return View(employee);
         }
+
+        public IActionResult Create() => View("Edit", new EmployeeViewModel());
+
+        #region Edit
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+                return View(new EmployeeViewModel());
+            
+            var employee = _EmployeesData.GetById((int)id);
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            var employee = new Employee()
+            {
+                Id = model.Id,
+                FirstName = model.Name,
+                LastName = model.LastName,
+                Patronymic = model.Patronymic,
+                Age = model.Age,
+            };
+
+            if (employee.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Update(employee);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region Delete
+
+        public IActionResult Delete(int id)
+        {
+            if (id < 0) return BadRequest();
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteComfirmed(int id)
+        {
+            _EmployeesData.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
     }
 }
