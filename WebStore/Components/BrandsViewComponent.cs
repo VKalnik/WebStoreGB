46a08577342eeb9301038179572c851eaece24e0
Victor Kalnik <v.kalnik@mail.ru>
2021-09-27 16:54:14 +0300

    Заготовки визуальных компонент секций и брендов

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+
+        public IViewComponentResult Invoke() => View();
+
+    }
+}
