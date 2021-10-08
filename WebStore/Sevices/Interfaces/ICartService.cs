db56061595d64abafcf0b44828620cf855ef5087
Victor Kalnik <v.kalnik@mail.ru>
2021-10-08 17:02:44 +0300

    Описание интерфейса сервиса корзины

---
+++ WebStore/Sevices/Interfaces/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using WebStore.ViewModels;
+
+namespace WebStore.Sevices.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int Id);
+
+        void Decriment(int Id);
+
+        void Remove(int Id);
+
+        void Clear();
+
+        CartViewModel GetViewModel();
+    }
+}
