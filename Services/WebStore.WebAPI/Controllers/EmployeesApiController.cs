7da399416364dda1980d2e7433cdcc6967ed9129
Victor Kalnik <v.kalnik@mail.ru>
2021-10-18 15:50:08 +0300

    Проектирование WebAPI для управления сотрудниками

--- Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Models;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers
@@ -18,7 +19,36 @@ namespace WebStore.WebAPI.Controllers
             return Ok(employees);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null)
+                return NotFound();
+            return Ok(employee);
+        }
 
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            var id = _EmployeesData.Add(employee);
+            return CreatedAtAction(nameof(GetById), new { id }, employee);
+            //return Ok(id);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Employee employee)
+        {
+            _EmployeesData.Update(employee);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _EmployeesData.Delete(id);
+            return result ? Ok(true) : NotFound(false);
+        }
 
     }
 }
