452b733878dae796f110099fa99d6fd6afb44c4e
Victor Kalnik <v.kalnik@mail.ru>
2021-10-28 14:32:32 +0300

    Тест разрушающего изменения кода

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WebStore.Controllers
             
             switch (id)
             {
-                default: return Content($"Status code --- {id}");
+                default: return Content($"Status code ---{id}");
                 case "404": return View("Error404");
             }
             
