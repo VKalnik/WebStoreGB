a95d102268d49dd3e5588ae5ccfe39b4520ad9c8
Victor Kalnik <v.kalnik@mail.ru>
2021-11-10 21:39:14 +0300

    Модернизация таг-хелпера переключалки страниц

--- UI/WebStore/TagHelpers/Pageing.cs
+++ UI/WebStore/TagHelpers/Pageing.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using WebStore.Domain.ViewModels;
 
 namespace WebStore.TagHelpers
 {
     public class Pageing : TagHelper
     {
-        private readonly IUrlHelperFactory _UrlHelperFactory;
+        //private readonly IUrlHelperFactory _UrlHelperFactory;
 
         public string PageAction { get; set; }
 
@@ -23,21 +25,21 @@ namespace WebStore.TagHelpers
         [ViewContext, HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
         
-        public Pageing(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
+        //public Pageing(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var ul = new TagBuilder("ul");
             ul.AddCssClass("pagination");
 
-            var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
+            //var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
             for (var i = 1; i <= PageModel.TotalPage; i++)
-                ul.InnerHtml.AppendHtml(CreateElement(i, url_helper));
+                ul.InnerHtml.AppendHtml(CreateElement(i/*, url_helper*/));
 
             output.Content.AppendHtml(ul);
         }
 
-        private TagBuilder CreateElement(int PageNumber, IUrlHelper Url)
+        private TagBuilder CreateElement(int PageNumber/*, IUrlHelper Url*/)
         {
             var li = new TagBuilder("li");
             var a = new TagBuilder("a");
@@ -47,10 +49,16 @@ namespace WebStore.TagHelpers
                 li.AddCssClass("active");
             else
             {
-                PageUrlValues["page"] = PageNumber;
-                a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
+                //a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
+                a.Attributes["href"] = "#";
             }
 
+            PageUrlValues["page"] = PageNumber;
+
+            foreach (var (key, value) in PageUrlValues.Select(v => (v.Key, Value: v.Value.ToString())).Where(v => v.Value?.Length > 0))
+                //if (value.ToString() is { Length: > 0 } str_value)
+                a.MergeAttribute($"data-{key}", value);
+            
             li.InnerHtml.AppendHtml(a);
             return li;
         }
