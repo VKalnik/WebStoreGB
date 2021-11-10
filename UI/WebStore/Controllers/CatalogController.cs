2b48940027916b326b551fda57a57f13f90beb1f
Victor Kalnik <v.kalnik@mail.ru>
2021-11-10 20:18:34 +0300

    Формирование html-фрагмента для скрипта

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using WebStore.Domain;
@@ -10,6 +11,9 @@ namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
+        private const string __PageSizeConfig = "CatalogPageSize";
+
+
         private readonly IProductData _ProductData;
         private readonly IConfiguration _Configuration;
 
@@ -22,7 +26,7 @@ namespace WebStore.Controllers
         public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
         {
             var page_size = PageSize
-                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value)
+                ?? (int.TryParse(_Configuration[__PageSizeConfig], out var value)
                     ? value
                     : null);
             
@@ -60,6 +64,25 @@ namespace WebStore.Controllers
                 return NotFound();
 
             return View(product.ToView());
+        }
+
+        public IActionResult GetProductsView(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
+        {
+            var products = GerProducts(BrandId, SectionId, Page, PageSize);
+
+            return PartialView("Partial/_Products", products);
+        }
+
+        private IEnumerable<ProductViewModel> GerProducts(int? BrandId, int? SectionId, int Page, int? PageSize = null)
+        {
+            var products = _ProductData.GetProducts(new()
+                {
+                    BrandId = BrandId,
+                    SectionId = SectionId,
+                    Page = Page,
+                    PageSize = PageSize ?? _Configuration.GetValue(__PageSizeConfig, 6)
+                });
+            return products.Products.OrderBy(p => p.Order).ToView();
         }
     }
 }
